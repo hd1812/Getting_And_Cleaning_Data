d9626557eeaf3c153bf2d09de42ef7aee9430616
hd1812 <hao.ding12@imperial.ac.uk>
2015-07-07 23:28:39 +0100

    Week 1

---
+++ Intro.R
@@ -0,0 +1,8 @@
+##Lecture notes
+##
+##Raw data > Processing script >tidy data>data analysis>data communication
+##This lecture is focused on the first three steps
+
+##Row and Processed data
+##Raw data might only need to be processed once, all steps should be recorded for downstream to refer to
+##
